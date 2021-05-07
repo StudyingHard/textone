0f6ba5b6d24e55fe4a9ab425f4edf9d47164c578
StudyingHard <398166254@qq.com>
2021-05-07 16:54:35 +0800

    初始化项目

---
+++ src/views/Home.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="home"  >
+    <h3 >首页</h3>
+    <router-link to="/login" @click="changedis" :class="{acticve: ac}">登录界面</router-link>
+  </div>
+</template>
+<script>
+
+// import { ref,reactive } from 'vue';
+export default {
+  name: "Home",
+  components: {},
+  data(){
+    return{
+      ac:false
+    }
+  },
+  methods:{
+    changedis(){
+      this.ac=true
+      console.log(ac);
+    }
+  }
+  // setup(){
+  //   const ac = ref<boolean>(false);
+  //   const changedis =reactive(()=>{
+  //     this.ac=!this.ac
+  //     console.log(ac);
+  //   })
+  // }
+ 
+};
+</script>
+<style scoped>
+</style>>
